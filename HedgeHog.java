17ecd0a3681a15de9e6e2bf78d8c60995ca535be
mariaruizgomez <crg.7886@gmail.com>
2023-03-28 14:11:49 -0700

    Update HedgeHog.java

--- HedgeHog.java
+++ HedgeHog.java
@@ -1,3 +1,23 @@
-public class HedgeHog {
+//make HedgeHog inherit from NocturnalAnimals
+public class HedgeHog extends NocturnalAnimals{
     
-}
+    //How would I indicate that the sound and eat methods have 
+    //been inherited from another class and given a new implementation? 
+    //What form of polymorphism is this?
+    @Override
+    public void sound(){
+        setSound("Grunting and Snuffling");
+        System.out.println("I use" + getSound() + "to communicate");
+    }
+     
+    @Override
+    public void eat(){
+        setFindPrey("Utilizing my nose.");
+        System.out.println("I eat bugs and fing meals by" + getFindPrey());
+    }
+
+    //Override the sleep method and have it say something that hedgehog would say
+    //@Override
+
+    }
+
