859b60cede824fcf110c5581b006957e565d9ca7
mariaruizgomez <crg.7886@gmail.com>
2023-03-28 10:17:35 -0700

    Animal interface

---
+++ Animals.java
@@ -0,0 +1,10 @@
+//Abstraction
+//Make an interdace called Animals
+public interface Animals {
+    public void sound();
+    public void eat();
+    public void sleep();
+}
+
+// Create 3 public abstract methods with no return type and no parameters called 
+// sound, eat, and sleep.
